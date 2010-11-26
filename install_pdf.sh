a126456583ad8c78da65ee9f36966bd240dba6f3
Neil Armstrong <superna9999@gmail.com>
2010-11-26 10:37:08 +0100

    Add mkdirs

--- install_pdf.sh
+++ install_pdf.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ ! -d $DESTDIR/usr/share/cups/mime ] && mkdir -p $DESTDIR/usr/share/cups/mime
 cp urftopdf.types  $DESTDIR/usr/share/cups/mime
 cp urftopdf.convs  $DESTDIR/usr/share/cups/mime
-cp urftopdf $DESTDIR/usr/lib/cups/filter/
+[ ! -d $DESTDIR/usr/lib/cups/filter ] && mkdir -p $DESTDIR/usr/lib/cups/filter
+cp urftopdf $DESTDIR/usr/lib/cups/filter
